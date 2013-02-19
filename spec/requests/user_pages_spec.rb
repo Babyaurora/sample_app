9c1c06faa7530616513f5839b298aea26ac1dd5c
Aurora <babyaurora@gmail.com>
2013-02-19 14:58:55 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 include ApplicationHelper
 
 describe "UserPages" do
-  
+
   subject { page }
-  
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
@@ -13,14 +13,14 @@ describe "UserPages" do
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
-  
+
   describe "signup page" do
     before { visit signup_path }
-    
+
     it { should have_selector('h1', text: 'Sign Up') }
     it { should have_selector('title', text: full_title('Sign Up')) }
   end
-  
+
   describe "signup" do
     before { visit signup_path }
     let(:submit) { "Create my account" }
@@ -29,7 +29,7 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-      
+
       describe "after submission" do
         before { click_button submit }
         it { should have_selector('title', text: 'Sign Up') }
@@ -48,13 +48,19 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-      
+
       describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
-        
+
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome')}
+        it { should have_link('Sign out') }
+
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link "Sign in" }
+        end
       end
     end
   end
